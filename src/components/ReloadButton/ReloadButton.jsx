bc33c7149ade2d89834d63d4504b34c11b3bf50a
Gospodin <gchav@softserveinc.com>
2019-12-01 18:21:59 +0200

    updating reload button component

--- src/components/ReloadButton/ReloadButton.jsx
+++ src/components/ReloadButton/ReloadButton.jsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IconButton } from '@material-ui/core';
 import ArrowRightTwoToneIcon from '@material-ui/icons/ArrowRightTwoTone';
 import HourglassEmptyTwoToneIcon from '@material-ui/icons/HourglassEmptyTwoTone';
 
 require('./ReloadButton.scss');
 
-const ReloadButton = ({ timer }) => {
+const ReloadButton = ({}) => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
+
+  const reset = () => {
+    setSeconds(0);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    }
+    if (seconds === 10) {
+      reset();
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
   return (
-    <div className="btn">
+    <div className="reload">
       <div className="reload-button">
-        <IconButton>
+        <IconButton onClick={toggle} disabled={isActive}>
           Reload
           <ArrowRightTwoToneIcon />
         </IconButton>
       </div>
-      <div className="progress-bar">
-        <HourglassEmptyTwoToneIcon />
-        <p>{timer}</p>
-      </div>
+      {isActive ? (
+        <div className="progress-bar">
+          <HourglassEmptyTwoToneIcon />
+          <p>{seconds}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
